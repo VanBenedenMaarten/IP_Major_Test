e2baac51f436f3d7c31fee1acc2cca0338e5851f
VanBenedenMaarten <maarten.vanbeneden@student.ucll.be>
2020-02-26 12:24:17 +0100

    fixed db

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :testdb, Testdb.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :testdb_web, TestdbWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 3306],
   server: false
 
 # Print only warnings and errors during test
